ceb04ebd7dff7bc624112d03f3387555cf6cc849
p4lad1n030 <pedrosantosmaciel@gmail.com>
2023-02-27 11:19:35 -0300

    up

---
+++ funcoes/callback_4filter.js
@@ -0,0 +1,22 @@
+const carrinho = [
+  { nome: "Caneta", qtd: 10, preco: 7.99 },
+  { nome: "Impressora", qtd: 0, preco: 649.5 },
+  { nome: "Caderno", qtd: 4, preco: 27.1 },
+  { nome: "Lapis", qtd: 3, preco: 5.82 },
+  { nome: "Tesoura", qtd: 1, preco: 19.2 },
+];
+
+const getNome = (item) => item.nome;
+const qtdMaiorQzero = (item) => item.qtd > 0;
+const res = carrinho.filter(qtdMaiorQzero).map(getNome);
+console.log(res);
+
+// solução gerada por mim antes do professor fazer
+// a minha solução não retorna um array como a do pr
+const maiorQZ = (item) => {
+  if (item.qtd > 0) {
+    let res = item.nome;
+    return console.log(res);
+  }
+};
+carrinho.filter(maiorQZ);
--- funcoes/conhecimento.js
+++ funcoes/conhecimento.js
@@ -4,3 +4,11 @@
 () => {
   return console.log("outra função de seta");
 };
+
+// map function
+/*gera um NOVO ARRAY com elementos transformados */
+const n = [1,2,3,4,5]
+const dobro = n1 => n1 * 2
+console.log(n.map(dobro))
+
+// filter function
